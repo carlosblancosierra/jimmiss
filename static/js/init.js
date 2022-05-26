f04a979b1762258a6b6e149cc7dd1f928e812c87
carlosblancomarketing <carlosblancosierra@gmail.com>
2022-05-26 15:29:36 +0200

    Feat: Add nav account dropdown

--- static/js/init.js
+++ static/js/init.js
@@ -1,5 +1,6 @@
 M.AutoInit();
 
 document.addEventListener('DOMContentLoaded', function() {
-
+    var navDropdown = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(navDropdown, ({alignment:'left','hover':true, 'constrainWidth':false, 'coverTrigger':false}));
 });
--- static/js/main.js
+++ static/js/main.js
@@ -13,6 +13,4 @@ $(document).ready(function() {
         dotsContainer: '#carousel-custom-dots'
     })
 
-
-
 });
