18fdbbf05f1193f5179eeaf6d313b9037968e6c0
carlosblancomarketing <carlosblancosierra@gmail.com>
2022-05-30 23:07:10 +0200

    Feat: aws new config

--- jimmiss/settings/local.py
+++ jimmiss/settings/local.py
@@ -14,10 +14,8 @@ from pathlib import Path
 
 import os
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -30,7 +28,6 @@ DEBUG = True
 STATIC_LOCAL = False
 
 ALLOWED_HOSTS = ['jimmiss.herokuapp.com', '127.0.0.1']
-
 
 # Application definition
 
@@ -54,7 +51,6 @@ INSTALLED_APPS = [
     'series',
     'skus',
     'tallas',
-
 ]
 
 LOGIN_URL = '/login'
@@ -92,7 +88,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'jimmiss.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -102,7 +97,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -122,7 +116,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -135,7 +128,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
