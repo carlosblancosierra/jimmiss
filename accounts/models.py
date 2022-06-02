495a5d1eb2ddd59f766f272250c8c103680aaf83
carlosblancomarketing <carlosblancosierra@gmail.com>
2022-06-02 17:04:10 +0200

    Feat: New user registration with details page

--- accounts/models.py
+++ accounts/models.py
@@ -1,13 +1,16 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+
 
 # Create your models here.
-User = settings.AUTH_USER_MODEL
 
 
 class UserDetails(models.Model):
-    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=255, blank=True, null=True)
     nombre_comercial = models.CharField(max_length=255, blank=True, null=True)
     razon_social = models.CharField(max_length=255, blank=True, null=True)
@@ -17,24 +20,7 @@ class UserDetails(models.Model):
     is_active = models.BooleanField(default=True)  # can login
     timestamp = models.DateTimeField(auto_now_add=True)
 
-    #
-    #     # confirm     = models.BooleanField(default=False)
-    #     # confirmed_date     = models.DateTimeField(default=False)
-    #
     def __str__(self):
         return self.user.username
-#
-#     def get_full_name(self):
-#         if self.full_name:
-#             return self.full_name
-#         return self.email
-#
-#     @property
-#     def is_staff(self):
-#         if self.is_admin:
-#             return True
-#         return self.staff
-#
-#     @property
-#     def is_admin(self):
-#         return self.admin
+
+
--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect
 from django.contrib.admin.views.decorators import staff_member_required
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.contrib import messages
 
 from orders.models import Order
 from addresses.models import Address
@@ -11,17 +12,40 @@ from addresses.models import Address
 from skus.models import SkuMaster, SkuProduct
 from marcas.models import Marca
 from .models import UserDetails
+from .forms import CustomUserCreationForm, UserDetailsForm
 
 User = settings.AUTH_USER_MODEL
 
 
-def register_page_local(request):
-    form = UserCreationForm(request.POST or None)
-    if form.is_valid():
-        user_obj = form.save()
-        return redirect('/login')
-    context = {"form": form}
-    return render(request, "accounts/register-local.html", context)
+# def register_page_local(request):
+#     form = UserCreationForm(request.POST or None)
+#     print(form)
+# if form:
+#     full_name = form.data['full_name']
+#     nombre_comercial = form.data['nombre_comercial']
+#     razon_social = form.data['razon_social']
+#     telefono = form.data['telefono']
+#     email = form.data['email']
+#     password1 = form.data['password1']
+#     password2 = form.data['password1']
+#
+#     # print('full_name: ', full_name)
+#     # print('nombre_comercial: ', nombre_comercial)
+#     # print('razon_social: ', razon_social)
+#     # print('telefono: ', telefono)
+#     # print('email: ', email)
+#     # print('password1: ', password1)
+#     # print('password2: ', password2)
+#
+#     user_obj = User(username=email,
+#                     email=email,
+#                     password=password1)
+#
+#     print(user_obj)
+#
+#     return redirect('/login')
+# context = {"form": form}
+# return render(request, "accounts/register-local.html", context)
 
 
 # Create your views here.
@@ -80,3 +104,36 @@ def home_page(request):
 
     }
     return render(request, "accounts/home.html", context)
+
+
+def register_page_local(request):
+    if request.method == 'POST':
+        user_form = CustomUserCreationForm(request.POST or None)
+        profile_form = UserDetailsForm(request.POST)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_obj = user_form.save()
+            print(user_obj)
+
+            nombre_comercial = profile_form.data['nombre_comercial']
+            razon_social = profile_form.data['razon_social']
+            telefono = profile_form.data['telefono']
+
+            user_details_obj = UserDetails(user=user_obj,
+                                           nombre_comercial=nombre_comercial,
+                                           razon_social=razon_social,
+                                           telefono=telefono,
+                                           )
+
+            user_details_obj.save()
+
+            messages.success(request, 'Your profile was successfully updated!')
+            return redirect('/')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        user_form = CustomUserCreationForm()
+        profile_form = UserDetailsForm()
+    return render(request, 'accounts/register-local.html', {
+        'user_form': user_form,
+        'profile_form': profile_form
+    })
