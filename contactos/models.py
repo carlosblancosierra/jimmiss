c9688293253c5da039cd1c60b2a275a1f5f204d4
carlosblancomarketing <carlosblancosierra@gmail.com>
2022-08-04 18:31:32 +0200

    Feat: contactos app

---
+++ contactos/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+# Create your models here.
+class Contacto(models.Model):
+    name = models.CharField(max_length=255, blank=True, null=True)
+    email = models.CharField(max_length=255, blank=True, null=True)
+    nombre_comercial = models.CharField(max_length=255, blank=True, null=True)
+    direccion_comercial = models.CharField(max_length=255, blank=True, null=True)
+    razon_social = models.CharField(max_length=255, blank=True, null=True)
+    telefono = models.CharField(max_length=255, blank=True, null=True)
+    comments = models.TextField(max_length=1200, blank=True, null=True)
+
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
