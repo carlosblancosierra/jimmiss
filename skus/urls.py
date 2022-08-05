de6bffb1393f48575a90d953dbca4f1f17268d4c
carlosblancomarketing <carlosblancosierra@gmail.com>
2022-08-05 22:59:24 +0200

    Feat: catalogo, sku

--- skus/urls.py
+++ skus/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'skus'
 
 urlpatterns = [
-    path('', views.all_page, name="list"),
+    path('', views.all_page, name="all"),
     path('<str:division_code>', views.list_page, name="list"),
     path('detalle/<str:slug>', views.detail_page, name="detail"),
 ]
