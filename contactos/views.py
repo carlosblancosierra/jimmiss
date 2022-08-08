f6011a0f91a311abacd44d9a8d53ab88c669992d
carlosblancomarketing <carlosblancosierra@gmail.com>
2022-08-08 15:30:43 +0200

    Feat: contactos views @staff_member_required

--- contactos/views.py
+++ contactos/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.contrib.admin.views.decorators import staff_member_required
 from .models import Contacto
 
 
 # Create your views here.
+@staff_member_required
 def detail_page(request, contact_id):
     contact = None
     qs = Contacto.objects.filter(id=contact_id)
@@ -17,6 +19,7 @@ def detail_page(request, contact_id):
     return render(request, "contactos/detail.html", context)
 
 
+@staff_member_required
 def home_page(request):
     qs = Contacto.objects.all()
 
